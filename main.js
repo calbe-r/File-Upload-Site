0517a07b17da617672d76ef0b13b9ebb4d213e59
[Caleb Radik] <[laoda1144@gmail.com]>
2021-04-14 15:36:29 +0800

    4/14

--- main.js
+++ main.js
@@ -2,14 +2,32 @@ const express = require("express");
 const path = require('path')
 const fileUpload = require('express-fileupload');
 const app = express()	
+const MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose')
+const uri = "mongodb+srv://admin:Password123@cluster0.wt1jg.mongodb.net/Test?retryWrites=true&w=majority"
 console.log("Hello World")
 
+//Start up the Website
 app.get('/',(req,res)=>{
 	res.sendFile(__dirname+'/FUS/index.html',console.error)
 })
 
+//Forces the program to create the pathways needed to upload the File
 app.use(fileUpload({createParentPath: true}));
 
+MongoClient.connect(uri, function (err, client) {
+  if (err) throw err
+
+  const db = client.db('Test')
+
+  db.collection('messages').find().toArray(function (err, result) {
+    if (err) throw err
+
+    console.log(result)
+  })
+})
+
+//Handles the file uploads
 app.post('/FUS/uploads', function(req,res) {
 	let sampleFile;
 	let uploadPath;
